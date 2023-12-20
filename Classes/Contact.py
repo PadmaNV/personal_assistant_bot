9ea8212e4151df3eec5e250c07cbd83b23250860
PadmaNV <146276005+PadmaNV@users.noreply.github.com>
2023-12-20 17:09:13 +0100

    del all

--- Classes/Contact.py
+++
@@ -1,7 +0,0 @@
-class Contact:
-    def __init__(self, name, address, phone_number, email, birthday):
-        self.name = name
-        self.address = address
-        self.phone_number = phone_number
-        self.email = email
-        self.birthday = birthday
