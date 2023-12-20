831183840fcbba58afef98415d43a18d5aaac449
PadmaNV <146276005+PadmaNV@users.noreply.github.com>
2023-12-20 22:09:20 +0200

    Update Contact.py

--- Classes/Contact.py
+++ Classes/Contact.py
@@ -1,11 +1,3 @@
-class Contact:
-    def __init__(self, name, address, phone_number, email, birthday):
-        self.name = name
-        self.address = address
-        self.phone_number = phone_number
-        self.email = email
-        self.birthday = birthday
-
 from .Field import Field
 from .Name import Name
 
