68d3a1423ce7208a77100ae7589e089cc0b702e7
PadmaNV <146276005+PadmaNV@users.noreply.github.com>
2023-12-24 11:58:13 +0200

    Revert "add phone add email add menu"

--- models/methods.py
+++ models/methods.py
@@ -1,6 +1,8 @@
-from models.custom_errors import *
 from models import *
-from main import change_contact_menu, main
+from models.custom_errors import *
+from prompt_toolkit import prompt,completion
+from .classes import *
+from main import change_contact_menu
 
 new_book = AddressBook()
 new_book.load_from_disk()
@@ -28,7 +30,7 @@ def all_contacts():
 def find_contact(name):
     found_contact = new_book.find(name)
     if found_contact == False:
-        raise KeyError
+        raise KeyError('Need to rework')
     else:
         return found_contact
 
@@ -42,7 +44,7 @@ def add_contact(args):
         if phone.isnumeric() and len(phone) == 10:
             break
         else:
-            print("Невірний номер.Номер повинен бути з 10 цифр")
+            print("Invalid phone number. Please enter a 10-digit numeric phone number.")
 
     while True:
         try:
@@ -60,7 +62,7 @@ def add_contact(args):
     
     while True:
         try:
-            notes = input("Вкажить які саме нотатки бажаєте додати: ")                
+            notes = input("Нотатки: ")                
             break
         except BirthdayFormat as e:
             print(f"Error: {e}")
@@ -70,83 +72,67 @@ def add_contact(args):
         contact.add_phone(phone)
         contact.add_email(email.value)
         contact.add_birthday(birthday.value)
-        contact.add_notes(notes)
+        if notes != "":
+            contact.add_notes(notes)
         return f"The new phone number, email, and birthday for the contact {name} successfully added."
     else:        
         new_contact = Record(name)
         new_contact.add_phone(phone)
         new_contact.add_email(email.value)
         new_contact.add_birthday(birthday.value)
-        new_contact.add_notes(notes)
+        if notes != "":
+            new_contact.add_notes(notes)        
         new_book.add_record(new_contact)        
         return f"Contact {name} successfully added."
 
-
 def edit_phone(name):
     current_contact = find_contact(name)
-    print("Існуючі номери телефонів:")
+    print("Current phone numbers:")
     for i, phone in enumerate(current_contact.phones, 1):
         print(f"{i}. {phone.value}")
 
     while True:
         try:
-            phone_choice = int(input("Виберіть номер телефону для редагування (або введіть 0 для додавання нового): "))
-            if 0 <= phone_choice <= len(current_contact.phones):
+            choice = int(input("Select a phone number to edit (or 0 to go back): "))
+            if 0 <= choice <= len(current_contact.phones):
                 break
             else:
-                print("Невірний вибір. Будь ласка, введіть правильний номер.")
+                print("Invalid choice. Please enter a valid number.")
         except ValueError:
-            print("Невірний ввід. Будь ласка, введіть номер.")
+            print("Invalid input. Please enter a number.")
 
-    if phone_choice == 0:
-        # Додаємо новий номер телефону
-        new_phone = input("Введіть новий номер телефону: ")
-        try:
-            current_contact.add_phone(new_phone)
-            print("Номер телефону успішно доданий.")
-        except WrongPhoneFormat:
-            print("Невірний формат номеру телефону. Будь ласка, введіть правильний номер.")
-    else:
-        # Редагуємо існуючий номер телефону
-        new_phone = input("Введіть новий номер телефону: ")
-        try:
-            current_contact.phones[phone_choice - 1] = Phone(new_phone)
-            print("Номер телефону успішно оновлено.")
-        except WrongPhoneFormat:
-            print("Невірний формат номеру телефону. Будь ласка, введіть правильний номер.")
+    if choice == 0:
+        return
+
+    new_phone = input("Enter the new phone number: ")
+    current_contact.phones[choice - 1] = Phone(new_phone)
+    print("Phone number successfully updated.")
 
 def edit_email(name):
     current_contact = find_contact(name)
-    print("Існуючі електронні адреси:")
-    for i, email in enumerate(current_contact.emails, 1):
+    print("Current email addresses:")
+    for i, email in enumerate(current_contact.email, 1):
         print(f"{i}. {email.value}")
 
     while True:
         try:
-            email_choice = int(input("Виберіть номер електронної адреси для редагування (або введіть 0 для додавання нової): "))
-            if 0 <= email_choice <= len(current_contact.emails):
+            choice = int(input("Select an email address to edit (or 0 to go back): "))
+            if 0 <= choice <= len(current_contact.email):
                 break
             else:
-                print("Невірний вибір. Будь ласка, введіть правильний номер.")
+                print("Invalid choice. Please enter a valid number.")
         except ValueError:
-            print("Невірний ввід. Будь ласка, введіть номер.")
+            print("Invalid input. Please enter a number.")
 
-    if email_choice == 0:
-        # Додаємо новий email
-        new_email = input("Введіть новий електронний адрес: ")
-        try:
-            current_contact.add_email(new_email)
-            print("Електронний адреса успішно додана.")
-        except WrongEmailFormat:
-            print("Невірний формат електронної адреси. Будь ласка, введіть правильний email.")
-    else:
-        # Редагуємо існуючий email
-        new_email = input("Введіть новий електронний адрес: ")
-        try:
-            current_contact.emails[email_choice - 1] = Email(new_email)
-            print("Електронний адреса успішно оновлена.")
-        except WrongEmailFormat:
-            print("Невірний формат електронної адреси. Будь ласка, введіть правильний email.")
+    if choice == 0:
+        return
+
+    new_email = input("Enter the new email address: ")
+    try:
+        current_contact.email[choice - 1] = Email(new_email)
+        print("Email address successfully updated.")
+    except WrongEmailFormat:
+        print("Invalid email address. Please enter a valid email.")
 
 def edit_birthday(name):
     current_contact = find_contact(name)
@@ -162,31 +148,47 @@ def edit_birthday(name):
         except WrongDataFormat:
             print("Invalid date format. Please enter the date in the format DD.MM.YYYY.")
 
+def validate_contact():    
+    print(all_contacts())
+    contacts = collect_contacts()
+    name_to_edit = prompt("Оберить ім'я контакту із списку вишче: ", completer=completion.WordCompleter(contacts))
+    if name_to_edit not in contacts:
+        raise KeyError("Ви ввели некоректне ім'я")
+    found_contact = find_contact(name_to_edit)
+    return found_contact
 
-@input_error
-def change_contact(args):
-    name = args[0]
-    
-    change_contact_menu()
-    choice = input("Enter the number of the option you'd like to choose: ")
+def validate_note(name):
+    note_keys = []
+    for note in name.notes:
+        for key,value in note.items():
+            note_keys.append(key)
+    return note_keys
+   
+def edit_note(name):    
+    note_to_change = input("Обери номер нотатки яку треба змінити: ")
+    if int(note_to_change) not in validate_note(name):
+        raise KeyError("Ви ввели некоректний номер нотатки")
+    note_new_text = input("Вкажи новий текст нотатки: ")  
+    name.notes.edit_note(note_to_change,note_new_text)
+    return f"Нотатки під номером {note_to_change} успішно змінена"
 
-    if choice == "1":
-        edit_phone(name)
-    elif choice == "2":
-        edit_email(name)
-    elif choice == "3":
-        edit_birthday(name)
-    elif choice == "3":
-        edit_birthday(name)
+def delete_note(name):
+    note_to_delete = input("Обери номер нотатки яку треба видалити, для видалення усіх нотаток введить команду all: ")
+    if note_to_delete == 'all':
+        name.notes.delete_note(all_notes=True)
+        return "Усі нотатки успішно видалені"
+    elif int(note_to_delete) not in validate_note(name):      
+        raise KeyError("Ви ввели некоректний номер нотатки")
     else:
-        print("Повернення до головного меню")
-        main()
-
-    return ""
+        name.notes.delete_note(note=note_to_delete)    
+        return f"Нотатка №{note_to_delete} успішно видалена"
+    
 @input_error
-def add_notes(contact_name,new_notes):
-    Record.add_notes(contact_name,new_notes)
-
+def add_notes():    
+    found_contact =  validate_contact()
+    new_notes = input("Додайте нотатки: ")   
+    Record.add_notes(found_contact,new_notes)
+    return f"Нотатки були додані до контакту {found_contact}"
 
 
 @input_error
